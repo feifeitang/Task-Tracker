5e14b4f427bb8b35266e75aac1588a801f64f57e
fei <fei@ldeMacBook-Air.local>
2021-03-15 21:07:32 +0800

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+// rafce 快捷鍵
+
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text, onClick }) => {
+    return (
+    <button
+        onClick={onClick}
+        style={{ backgroundColor: color }}
+        className='btn'
+    >
+        {text}
+    </button>
+    )
+}
+
+Button.defaultProps = {
+    color: 'stellblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+export default Button
